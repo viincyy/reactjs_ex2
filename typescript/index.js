de9473b3903914ff6a3a00d90eb3ea925b322586
viincyy <viincyy126@gmail.com>
2021-08-26 00:39:39 +0700

    array object ts

--- typescript/index.js
+++ typescript/index.js
@@ -1,1 +1,11 @@
-console.log('123');
+// FUNCTION
+var square = function (side) { return side * side; };
+console.log(square(3));
+var greet;
+greet = function (a, b) { return a * b; };
+console.log(greet(1, 2));
+var add = function (a, b, c) {
+    console.log();
+};
+console.log(add(1, 2, 3));
+var minus = function (a, b) { return a - b; };
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,13 @@
-console.log('123');
+// FUNCTION
+const square= (side: number) => side*side
+console.log(square(3))
+
+let greet: Function
+greet = (a: number, b: number) => a*b;
+console.log(greet(1, 2))
+const add = (a: number, b: number, c:number | string) => {
+    console.log()
+}
+console.log(add(1,2,3))
+
+const minus = (a: number, b:number):number => a - b;
